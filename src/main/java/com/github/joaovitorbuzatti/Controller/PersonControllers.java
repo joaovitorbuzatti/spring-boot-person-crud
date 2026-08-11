40fbc3a7cb2c340e106de33c4348e9f8c6ff6ca6
joao vitor buzatti <joaovitorbuzatti9@gmail.com>
2026-08-11 17:38:38 -0300

    implementaçoes do delete, create e update

--- src/main/java/com/github/joaovitorbuzatti/Controller/PersonControllers.java
+++ src/main/java/com/github/joaovitorbuzatti/Controller/PersonControllers.java
@@ -1,8 +1,12 @@
 package com.github.joaovitorbuzatti.Controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +23,29 @@ public class PersonControllers {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-    public Person getPerson(@PathVariable("id") String id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id){
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return service.findAll();
     }
 
 }
--- src/main/java/com/github/joaovitorbuzatti/service/PersonService.java
+++ src/main/java/com/github/joaovitorbuzatti/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.github.joaovitorbuzatti.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -14,6 +16,20 @@ public class PersonService {
 
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+    public List<Person> findAll(){
+        logger.info("Finding all person!");
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++){
+
+            Person person = mockPerson(i);
+            persons.add(person);
+
+        }
+
+        return persons;
+    }
+
     public Person findById(String id){
 
         logger.info("Finding one Person!");
@@ -23,6 +39,33 @@ public class PersonService {
         person.setFirstName("Joao vitor");
         person.setLastName("buzatti");
         person.setAddress("São jose do rio preto - SP - Brasil");
+        person.setGender("Male");
+
+        return person;
+
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("updating one person!");
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting one person!");
+    }
+
+    private Person mockPerson(int i) {
+        
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("firstName" + i);
+        person.setLastName("lastname" + i);
+        person.setAddress("some address in brasil" + i);
         person.setGender("Male");
 
         return person;
